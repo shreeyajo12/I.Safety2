65d389cec10f1986dd936073991426f2883e1127
Shivam <iam.shivamsng@gmail.com>
2019-11-04 12:53:18 +0530

    Suman panic button.

--- app/src/main/java/com/example/isafety/HomePage.java
+++ app/src/main/java/com/example/isafety/HomePage.java
@@ -7,6 +7,11 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -16,6 +21,31 @@ public class HomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+
+        final Button customButton = findViewById(R.id.custom_button);
+        Switch switchEnabledButton = findViewById(R.id.switch_enable_button);
+
+        customButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(HomePage.this, "Click", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        switchEnabledButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    customButton.setEnabled(true);
+                }else{
+                    customButton.setEnabled(false);
+                }
+
+            }
+        });
+
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
